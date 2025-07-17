98d33c3d33000679344500cec244adbf0ee3ed34
MaximizeAy <ayomide.daniel.adeosun@gmail.com>
2025-07-17 18:29:37 +0100

    Please I'm tired

--- Test v2.0.0/js/main.js
+++ Test v2.0.0/js/main.js
@@ -3,11 +3,6 @@ function toggleMenu() {
   nav.classList.toggle('show');
 }
 
-function toggleBlog(btn, id) {
-const content = document.getElementById(id);
-content.classList.toggle('expanded');
-btn.textContent = content.classList.contains('expanded') ? 'Read Less' : 'Read More';
-}
 
  // SEAT SELECTION FUNCTIONS
         let selectedMovie = '';
